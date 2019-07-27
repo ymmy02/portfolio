49b3ef2e602291c133f6a8475765b1e0df06d81c
Kohei Yamamoto <kh2yam1@gmial.com>
2019-07-27 09:55:42 -0400

    Add title logo

--- portfolio/src/components/TitleLogo.vue
+++ portfolio/src/components/TitleLogo.vue
@@ -1,11 +1,13 @@
 <template>
     <v-layout align-center justify-center>
-    <v-img
-      src="../assets/title-logo.png"
-      lazy-src="../assets/title-logo.png"
-      max-width="500"
-      max-height="300"
-    ></v-img>
+        <div class="title">
+            <v-img
+            src="../assets/title-logo.png"
+            lazy-src="../assets/title-logo.png"
+            max-width="500"
+            max-height="300"
+            ></v-img>
+        </div>
   </v-layout>
 </template>
 
@@ -14,9 +16,15 @@ export default {
     name: "TitleLogo",
     data:() => {
         return {
-            logo: require('@/assets/title-logo.png'),
         }
     }
 }
 </script>
+
+<style scoped>
+.title {
+    height: 500px;
+    padding-top: 200px;
+}
+</style>
 
