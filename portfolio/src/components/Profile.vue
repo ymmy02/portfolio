32cbc767f4f7a3b0d5dc8ab45b4a5c4ac82030c2
Kohei Yamamoto <kh2yam1@gmial.com>
2019-07-27 16:42:45 -0400

    Implementing Profile content...

--- portfolio/src/components/Profile.vue
+++ portfolio/src/components/Profile.vue
@@ -1,10 +1,39 @@
 <template>
-    <h1>Profile</h1>
+    <div class="profile">
+        <h2 class="content-title font-weight-regular mt-4 ml-4">
+            <v-icon>mdi-account-circle-outline</v-icon>
+            Profile
+        </h2>
+        <v-layout align-center justify-center>
+            <p class="text">{{ text }}</p>
+        </v-layout>
+    </div>
 </template>
 
 <script>
 export default {
     name: 'Profile',
+    data: () => {
+        return {
+            text: `
+                こんにちは
+                はじめまして
+                山本と言います。
+            `
+        }
+    }
 }
 </script>
+
+<style scoped>
+.profile {
+    padding: auto;
+    /* height: 500px; */
+}
+
+.text {
+    white-space: pre-wrap;
+    word-wrap:break-word;
+}
+</style>
 
