187aec6d2f2d7a65a86022c9cd1c4d1718a0ec32
Kohei Yamamoto <kh2yam1@gmial.com>
2019-07-28 15:03:23 -0400

    Add SkillCard

--- portfolio/src/components/Skill.vue
+++ portfolio/src/components/Skill.vue
@@ -1,9 +1,31 @@
 <template>
-    <h1>Skill</h1>
+    <div class="skill">
+        <h2 class="content-title font-weight-regular mt-4 ml-4">
+            <v-icon>mdi-laptop</v-icon>
+            Skill
+        </h2>
+        <v-layout wrap>
+            <SkillCard v-for="card in cards" :key="card.title" />
+        </v-layout>
+    </div>
 </template>
 
 <script>
+import SkillCard from './SkillCard';
+
 export default {
     name: 'Skill',
+    components: {
+        SkillCard
+    },
+    data: () => {
+        return {
+            cards: [
+                {title: 'Programming Language'},
+                {title: 'Backend'},
+                {title: 'Frontend'}
+            ]
+        }
+    }
 }
 </script>
