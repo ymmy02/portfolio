3e77195caee40d8035cc76869742944700b66449
Kohei Yamamoto <kh2yam1@gmial.com>
2019-07-29 10:54:17 -0400

    Implementing Hobby...

--- portfolio/src/components/Hobby.vue
+++ portfolio/src/components/Hobby.vue
@@ -1,9 +1,55 @@
 <template>
-    <h1>Hobby</h1>
+    <div class="hobby">
+        <h2 class="content-title font-weight-regular mt-4 ml-4">
+            <v-icon>mdi-heart-outline</v-icon>
+            Hobby
+        </h2>
+        <HobbyItem 
+            v-for="item in items"
+            :key="item.title"
+            :icon="item.icon"
+            :title="item.title"
+            :comment="item.comment"
+            :image="item.image"
+        />
+    </div>
 </template>
 
 <script>
+import HobbyItem from './HobbyItem';
+
 export default {
     name: 'Hobby',
+    components: {
+        HobbyItem
+    },
+    data: () => ({
+        items: [
+            {
+                icon: 'mdi-karate',
+                title: '合気道',
+                comment: '週二回',
+                image: require('../assets/hobby/aikido.png'),
+            },
+            {
+                icon: 'mdi-apple-safari',
+                title: '旅行',
+                comment: '週二回',
+                image: require('../assets/hobby/travel.jpg'),
+            },
+            {
+                icon: 'mdi-beer',
+                title: 'ビール',
+                comment: '週二回',
+                image: require('../assets/hobby/beer.jpg'),
+            },
+            {
+                icon: 'mdi-chart-line',
+                title: '投資',
+                comment: '週二回',
+                image: require('../assets/hobby/investment.png'),
+            },
+        ]
+    })
 }
 </script>
---
+++ portfolio/src/components/HobbyItem.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="ma-4">
+        <h3 class="ml-5 fonto-weight-regular">
+            <v-icon>{{ icon }}</v-icon>
+            {{ title }}
+        </h3>
+        <v-layout align-center justify-space-around>
+            <p>{{ comment }}</p>
+            <v-img
+                :src="image"
+                :lazy-src="image"
+                class="mx-5"
+                max-width="250"
+                height="auto"
+            ></v-img>
+        </v-layout>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HobbyItem',
+    props: {
+        icon: String,
+        title: String,
+        comment: String,
+        image: String
+    }
+}
+</script>
+
